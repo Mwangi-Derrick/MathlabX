53d94f38efdb4549116017f9a345f8b3285ac251
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-07 01:48:37 +0300

    feat: funstion def for curl in 3D fields

--- cpp-engine/math/vector_math.h
+++ cpp-engine/math/vector_math.h
@@ -18,6 +18,11 @@ namespace VectorMath {
      double curl2D(const GridPoint2D& prevX, const GridPoint2D& nextX,
                          const GridPoint2D& prevY, const GridPoint2D& nextY,
                          double dx, double dy);
+     
+     double curl3D(const GridPoint3D& pX, const GridPoint3D& nX,
+                         const GridPoint3D& pY, const GridPoint3D& nY,
+                         const GridPoint3D& pZ, const GridPoint3D& nZ,
+                         double dx, double dy, double dz);
 
      double divergence3D(const GridPoint3D& pX, const GridPoint3D& nX,
                                const GridPoint3D& pY, const GridPoint3D& nY,
