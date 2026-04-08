fd2b0fdb5542ae67a1ff9709660b0af018214167
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-08 16:32:13 +0300

    feat: created point2d and point3d structs

--- cpp-engine/shared/structs.h
+++ cpp-engine/shared/structs.h
@@ -1,6 +1,19 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
+struct Point2D {
+    double x, y;
+    Point2D(double x_ = 0, double y_ = 0) : x(x_), y(y_) {}
+};
+
+struct Point3D {
+    double x, y, z;
+    double vx, vy, vz;
+    Point3D(double x_ = 0, double y_ = 0, double z_ = 0,
+            double vx_ = 0, double vy_ = 0, double vz_ = 0)
+        : x(x_), y(y_), z(z_), vx(vx_), vy(vy_), vz(vz_) {}
+};
+
 struct GridPoint2D{
     double x, y;
     double fx, fy; // The field vector at this point
