09e471bb87bcf6979f46c2a97515da7a1c0e10b9
Derrick Mwangi <mwangiderrick640@gmail.com>
2026-04-11 20:37:34 +0300

    chore: intergrated tailwindcss vite plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,10 +9,11 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/MathlabX/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     dedupe: ['react', 'react-dom']  // This ensures single React instance
   },
